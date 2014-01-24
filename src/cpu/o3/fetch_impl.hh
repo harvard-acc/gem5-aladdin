ef8ed543e12279b927800f697e4474227b71579d
Dam Sunwoo <dam.sunwoo@arm.com>
2014-01-24 15:29:30 -0600

    mem: per-thread cache occupancy and per-block ages
    This patch enables tracking of cache occupancy per thread along with
    ages (in buckets) per cache blocks.  Cache occupancy stats are
    recalculated on each stat dump.
    
    Change-Id: I92307a9d11b10be68410aafe773f74aa2b5c110d

--- src/cpu/o3/fetch_impl.hh
+++ src/cpu/o3/fetch_impl.hh
@@ -596,6 +596,8 @@ DefaultFetch<Impl>::fetchCacheLine(Addr vaddr, ThreadID tid, Addr pc)
                     Request::INST_FETCH, cpu->instMasterId(), pc,
                     cpu->thread[tid]->contextId(), tid);
 
+    mem_req->taskId(cpu->taskId());
+
     memReq[tid] = mem_req;
 
     // Initiate translation of the icache block
