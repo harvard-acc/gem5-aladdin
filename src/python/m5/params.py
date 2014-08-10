c6b016bd84d7ca5dfcc1646724de55dab79e2fac
Sophia Shao <shao@eecs.harvard.edu>
2014-08-10 19:11:24 -0400

    still stddef.h, fixed it fundamentally, is able to compile arm and x86

--- src/python/m5/params.py
+++ src/python/m5/params.py
@@ -286,6 +286,20 @@ class VectorParamDesc(ParamDesc):
         code('%module(package="m5.internal") ${{self.swig_module_name()}}')
         code('%{')
         self.ptype.cxx_predecls(code)
+        code('''\
+/**
+  * This is a workaround for bug in swig. Prior to gcc 4.6.1 the STL
+  * headers like vector, string, etc. used to automatically pull in
+  * the cstddef header but starting with gcc 4.6.1 they no longer do.
+  * This leads to swig generated a file that does not compile so we
+  * explicitly include cstddef. Additionally, including version 2.0.4,
+  * swig uses ptrdiff_t without the std:: namespace prefix which is
+  * required with gcc 4.6.1. We explicitly provide access to it.
+  */
+#include <cstddef>
+#include <stddef.h>
+using std::ptrdiff_t;
+''')
         code('%}')
         code()
         # Make sure the SWIGPY_SLICE_ARG is defined through this inclusion
