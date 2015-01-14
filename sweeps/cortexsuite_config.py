a39c976a239f6153f1a4a9c86914295cd8d03d90
Sam Xi <samxi@seas.harvard.edu>
2015-01-14 16:25:01 -0500

    Support for localization and sift benchmarks in CortexSuite.
    Change-Id: I10ee37e892de48b07048409407a223912703528a

--- sweeps/cortexsuite_config.py
+++ sweeps/cortexsuite_config.py
@@ -35,4 +35,32 @@ disparity.add_array("retDisp", 12290, 4, PARTITION_CYCLIC)
 disparity.generate_separate_kernels(separate=True, enforce_order=True)
 disparity.use_local_makefile()
 
-CORTEXSUITE = [disparity]
+localization = Benchmark("localization",
+                         "localization/src/c/script_localization")
+localization.set_kernels(["weightedSample_worker"])
+localization.add_loop("weightedSample_worker", 20, 3)
+localization.add_loop("weightedSample_worker", 22, 3)
+localization.add_loop("weightedSample_worker", 27, 3)
+localization.add_array("seed", 500, 4, PARTITION_CYCLIC)
+localization.add_array("bin", 500, 4, PARTITION_CYCLIC)
+localization.add_array("w", 500, 4, PARTITION_CYCLIC)
+localization.use_local_makefile()
+
+# Input size: sim
+sift = Benchmark("sift", "sift/src/c/script_sift")
+sift.set_kernels(["imsmooth_worker"])
+sift.add_array("temp", 27, 4, PARTITION_CYCLIC)
+# The sizes of these arrays change over the iterations, so I've specified the
+# largest size observed.
+sift.add_array("buffer", 64*64, 4, PARTITION_CYCLIC)
+sift.add_array("array", 64*64, 4, PARTITION_CYCLIC)
+sift.add_array("out", 64*64, 4, PARTITION_CYCLIC)
+sift.add_loop("imsmooth_worker", 62, UNROLL_FLATTEN)
+sift.add_loop("imsmooth_worker", 68, UNROLL_FLATTEN)
+sift.add_loop("imsmooth_worker", 77, UNROLL_ONE)
+sift.add_loop("imsmooth_worker", 79)
+sift.add_loop("imsmooth_worker", 101, UNROLL_ONE)
+sift.add_loop("imsmooth_worker", 103)
+sift.use_local_makefile()
+
+CORTEXSUITE = [disparity, localization, sift]
