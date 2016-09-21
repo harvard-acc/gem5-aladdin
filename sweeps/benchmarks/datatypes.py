312607a13c1250c26b2b8269cb3f80fe858d02a2
Sam Xi <samxi@seas.harvard.edu>
2016-09-21 11:07:12 -0400

    Xenon: fix bug where array size was not computed correctly.
    Added more documentation to prevent this from happening in the future.
    
    Change-Id: Ic81300a4f5315a632fc93b4e5b17d9ee31534498

--- sweeps/benchmarks/datatypes.py
+++ sweeps/benchmarks/datatypes.py
@@ -44,21 +44,56 @@ class Benchmark(Sweepable):
     self.run_args = ""
 
   def add_array(self, array_name, array_size, array_word_length, **kwargs):
+    """ Add an array of this benchmark.
+
+    Args:
+      array_name: Name of the array as it appears in the source.
+      array_size: Number of ELEMENTS in the array, NOT the number of bytes.
+      array_word_length: Bytes per ELEMENT.
+      **kwargs: See Array().
+    """
     array = Array(array_name, array_size, array_word_length, **kwargs)
     assert(not hasattr(self, array_name))
     setattr(self, array_name, array)
 
   def add_loop(self, function_name, loop_name, **kwargs):
+    """ Add a loop of this benchmark.
+
+    Args:
+      function_name: The name of the function that contains this loop.
+      loop_name: The label that marks the start of this loop.
+      **kwargs: See Loop()
+    """
     if not hasattr(self, function_name):
       f = Function(function_name)
       setattr(self, function_name, f)
     getattr(self, function_name).add_loop(loop_name, **kwargs)
 
   def set_kernels(self, kernels):
+    """ Set the kernels to be traced in this benchmark.
+
+    If a single kernel is provided, then all functions called by that function
+    will be called. If multiple kernels are provided, only those functions will
+    appear in the dynamic trace.
+
+    Args:
+      kernels: A list of function names.
+    """
     self.kernels = kernels
 
-  def set_main_id(self, id):
-    pass
+  def set_main_id(self, main_id):
+    """ Set the id number of this benchmark.
+
+    In a system with multiple accelerators, this allows the simulator to distinguish
+    between them.
+
+    TODO: Remove this and replace with a dynamic registration procedure
+    (BUG=ALADDIN-66).
+
+    Args:
+      main_id: integer id of this benchmark.
+    """
+    self.main_id = main_id
 
   # TODO: Leave off exec cmd for now.
 
