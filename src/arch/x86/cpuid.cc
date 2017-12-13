a22e3b8b8d6941cc8503c7c6c607c91c34aa35c3
Sam Xi <samxi@seas.harvard.edu>
2017-12-13 13:53:45 -0500

    arch: Temporary cpuid function 7 implementation.
    Function 7 returns deterministic cache parameters based on the value of
    ECX (subleaf).  Since we don't have a way to actually get the simulated
    system configuration yet, this just hardcodes the default parameters.
    
    Change-Id: I951caccd615162e9e577544cb4c7eebc20f45178

--- src/arch/x86/cpuid.cc
+++ src/arch/x86/cpuid.cc
@@ -37,7 +37,13 @@ namespace X86ISA {
     enum StandardCpuidFunction {
         VendorAndLargestStdFunc,
         FamilyModelStepping,
-        NumStandardCpuidFuncs
+        CacheAndTLBDescriptors,
+        ProcessorSerialNumber,
+        CoreAndCacheTopology,
+        Reserved5,
+        Reserved6,
+        ExtendedFeatures,
+        NumStandardCpuidFuncs,
     };
 
     enum ExtendedCpuidFunctions {
@@ -63,6 +69,74 @@ namespace X86ISA {
         NumExtendedCpuidFuncs
     };
 
+namespace CpuidCacheParams {
+    // Bit fields returned in EAX for leaf 4.
+
+    enum EAXCacheAttributes {
+        // Bits [4:0]
+        NoCache = 0x0,
+        DataCache = 0x1,
+        InstructionCache = 0x2,
+        UnifiedCache = 0x3,
+
+        SelfInitializing = 1 << 8,
+        FullyAssociative = 1 << 9,
+    };
+
+    enum EBXCacheAttributes {
+        WBInvNotActsOnLowerCaches = 0x1,
+        IsInclusiveCache = 0x2,
+        ComplexCacheIndexing = 0x4,
+    };
+
+    int getEAX(int subleaf) {
+        switch (subleaf) {
+            case 0:
+                return DataCache | (1 << 5) | SelfInitializing;
+            case 1:
+                return InstructionCache | (1 << 5) | SelfInitializing;
+            case 2:
+                return UnifiedCache | (2 << 5) | SelfInitializing;
+            case 3:
+                return UnifiedCache | (3 << 5) | SelfInitializing;
+            default:
+                return NoCache;
+        }
+    }
+
+    int getEBX(int cacheSize, int cacheLineSize, int associativity) {
+        if (cacheSize == 0)
+            return 0;
+        unsigned cacheLineSizeField = (cacheLineSize - 1) & (0xfff);
+        unsigned physicalLinePartitions = (cacheSize / cacheLineSize) & (0x3ff);
+        int ebx = cacheLineSizeField | (physicalLinePartitions << 12) |
+                  ((associativity - 1) << 22);
+        return ebx;
+    }
+
+    int getECX(int cacheSize, int cacheLineSize, int associativity) {
+        if (cacheSize == 0)
+            return 0;
+        return (cacheSize / associativity / cacheLineSize) - 1;
+    }
+
+
+    int getEDX(int subleaf) {
+        // By default, all caches are mostly inclusive, propagate wb
+        // invalidates, and do not use complex hashing.
+        switch (subleaf) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                return IsInclusiveCache;
+            default:
+                return 0;
+        }
+    }
+
+}  // namespace CpuidCacheParams
+
     static const int vendorStringSize = 13;
     static const char vendorString[vendorStringSize] = "M5 Simulator";
     static const int nameStringSize = 48;
@@ -77,6 +151,40 @@ namespace X86ISA {
             reg |= str[pos];
         }
         return reg;
+    }
+
+    CpuidResult getCacheParameters(ThreadContext* tc, int subleaf) {
+        using namespace CpuidCacheParams;
+
+        int eax = getEAX(subleaf);
+        int edx = getEDX(subleaf);
+        int ebx = 0, ecx = 0;
+        // TODO: For now, just encode the default parameters. We'll fix
+        // this up later.
+        unsigned cacheSize = 0, associativity = 0, cacheLineSize = 64;
+        switch (subleaf) {
+            case 0:
+            case 1:
+                cacheSize = 64 * 1024;
+                associativity = 2;
+                break;
+            case 2:
+                cacheSize = 2 * 1024 * 1024;
+                associativity = 8;
+                break;
+            case 3:
+                cacheSize = 16 * 1024 * 1024;
+                associativity = 16;
+                break;
+            default:
+                cacheSize = 0;
+                associativity = 1;
+                break;
+        }
+        ebx = getEBX(cacheSize, cacheLineSize, associativity);
+        ecx = getECX(cacheSize, cacheLineSize, associativity);
+
+        return CpuidResult(eax, ebx, edx, ecx);
     }
 
     bool
@@ -157,6 +265,13 @@ namespace X86ISA {
               case FamilyModelStepping:
                 result = CpuidResult(0x00020f51, 0x00000805,
                                      0xe7dbfbff, 0x04000209);
+                break;
+              case CoreAndCacheTopology:
+                result = getCacheParameters(tc, index);
+                break;
+              case ExtendedFeatures:
+                // No extended features are supported.
+                result = CpuidResult(0x0, 0x0, 0x0, 0x0);
                 break;
               default:
                 warn("x86 cpuid family 0x0000: unimplemented function %u",
