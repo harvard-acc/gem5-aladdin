dade56ce8c9ce0511c33afb9768447ad0961c09d
Yuan Yao <yuanyao@seas.harvard.edu>
2019-09-14 18:08:41 -0400

    systolic-array: Change the test to use float16 data.
    We will primarily use float16 data in our workloads, so turn the test case
    into using float16 data.
    
    Change-Id: Ie353ff2e53165bb1c9de721dca7638189f9ebcfd

--- src/systolic_array/test/test.c
+++ src/systolic_array/test/test.c
@@ -5,11 +5,17 @@
 
 #include "systolic_array_connection.h"
 #include "aladdin/gem5/aladdin_sys_connection.h"
+#include "FP16/include/fp16.h"
 
 #define CACHELINE_SIZE 32
 
+typedef uint16_t float16;
+
+float16 fp16(float fp32_data) { return fp16_ieee_from_fp32_value(fp32_data); }
+float fp32(float16 fp16_data) { return fp16_ieee_to_fp32_value(fp16_data); }
+
 int main() {
-  float *inputs, *weights, *outputs;
+  float16 *inputs, *weights, *outputs;
   int input_dims[4] = { 1, 16, 16, 8 };
   int weight_dims[4] = { 16, 3, 3, 8 };
   int output_dims[4] = { 1, 16, 16, 16 };
@@ -21,26 +27,26 @@ int main() {
     output_size *= output_dims[i];
   }
   int err = posix_memalign(
-      (void**)&inputs, CACHELINE_SIZE, sizeof(float) * input_size);
+      (void**)&inputs, CACHELINE_SIZE, sizeof(float16) * input_size);
   assert(err == 0 && "Failed to allocate memory!");
   err = posix_memalign(
-      (void**)&weights, CACHELINE_SIZE, sizeof(float) * weight_size);
+      (void**)&weights, CACHELINE_SIZE, sizeof(float16) * weight_size);
   assert(err == 0 && "Failed to allocate memory!");
   err = posix_memalign(
-      (void**)&outputs, CACHELINE_SIZE, sizeof(float) * output_size);
+      (void**)&outputs, CACHELINE_SIZE, sizeof(float16) * output_size);
   assert(err == 0 && "Failed to allocate memory!");
 
   int reset_counter = input_dims[3];
   float value = 0;
   for (int i = 0; i < input_size; i++) {
-    inputs[i] = value++;
+    inputs[i] = fp16(value++);
     if ((i + 1) % reset_counter == 0)
       value = 0;
   }
   value = 0;
   reset_counter = weight_dims[3];
   for (int i = 0; i < weight_size; i++) {
-    weights[i] = value++;
+    weights[i] = fp16(value++);
     if ((i + 1) % reset_counter == 0)
       value = 0;
   }
@@ -61,15 +67,15 @@ int main() {
   data.act_type = SYSTOLIC_RELU;
   int accelerator_id = 4;
   mapArrayToAccelerator(
-      accelerator_id, "", data.input_base_addr, input_size * sizeof(float));
+      accelerator_id, "", data.input_base_addr, input_size * sizeof(float16));
   mapArrayToAccelerator(
-      accelerator_id, "", data.weight_base_addr, weight_size * sizeof(float));
+      accelerator_id, "", data.weight_base_addr, weight_size * sizeof(float16));
   mapArrayToAccelerator(
-      accelerator_id, "", data.output_base_addr, output_size * sizeof(float));
+      accelerator_id, "", data.output_base_addr, output_size * sizeof(float16));
   invokeSystolicArrayAndBlock(accelerator_id, data);
 
   for (int i = 0; i < output_size; i++) {
-    printf("%.2f ", outputs[i]);
+    printf("%.2f ", fp32(outputs[i]));
     if ((i + 1) % output_dims[3] == 0)
       printf("\n");
     if ((i + 1) % (output_dims[2] * output_dims[3]) == 0)
