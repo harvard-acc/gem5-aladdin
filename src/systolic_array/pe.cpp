158440b36567c536a95435fa20962259e3ed6033
Yuan Yao <yaoyuannnn@gmail.com>
2020-06-08 11:53:02 -0400

    systolic-array: Fix a bug in the PE.
    Change-Id: I54dd071417a1a42644508bfdab50222b57d2ee93

--- src/systolic_array/pe.cpp
+++ src/systolic_array/pe.cpp
@@ -8,9 +8,10 @@ void MulAccUnit::checkEndOfWindow() {
   const std::vector<int>& weightIndices = input1->indices;
   if (weightIndices[1] == accel.weightRows - 1 &&
       weightIndices[2] == accel.weightCols - 1 &&
-      weightIndices[3] == accel.weightChans - 1)
+      weightIndices[3] == accel.weightChans - 1) {
     output->windowEnd = true;
     output->bubble = false;
+  }
 }
 
 // We can't directly do float16 operations, here we use a FP16 library for that.
