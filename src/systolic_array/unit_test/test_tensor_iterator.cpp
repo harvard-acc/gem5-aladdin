d817f12f0016a8e5570c85bf792ddfed95c5570d
Yuan Yao <yaoyuannnn@gmail.com>
2021-01-20 22:33:52 +0000

    systolic-array: Change the behavior-driven tests to SECTIONs.
    Change-Id: I5491a1c1af5d1967b56d46b683aacc0c29abcca4

--- src/systolic_array/unit_test/test_tensor_iterator.cpp
+++ src/systolic_array/unit_test/test_tensor_iterator.cpp
@@ -30,120 +30,99 @@ void doRegionTest(TensorIndexIterator& iter, const std::vector<int>& region) {
   REQUIRE(iter.getIndices() == refResults);
 }
 
-SCENARIO("Test tensor index iterator", "[tensor-index]") {
-  GIVEN("Test tensor shape without paddings.") {
+TEST_CASE("Test tensor index iterator", "[tensor-index]") {
+  SECTION("Tensor shape without paddings") {
     TensorShape shape({ 4, 8, 8, 16 });
     TensorIndexIterator iter(shape);
-    WHEN("Test single step advance.") {
-      THEN("Advance the iterator by 1 step.") {
+    SECTION("Single step advance") {
+      ++iter;
+      REQUIRE(int(iter) == 1);
+      for (int i = 0; i < 1234; i++)
         ++iter;
-        REQUIRE(int(iter) == 1);
-      }
-      THEN("Advance the iterator by a random number of single steps.") {
-        for (int i = 0; i < 1234; i++)
-          ++iter;
-        REQUIRE(int(iter) == 1234);
-      }
+      REQUIRE(int(iter) == 1235);
     }
-    WHEN("Test regin-based advance.") {
-      THEN("Advance the iterator by regions that don't go out of dimension "
-           "bounds.") {
+    SECTION("Region-based advance") {
+      SECTION("Advance a region that don't go out of dimension bounds") {
         doRegionTest(iter, { 1, 2, 3, 4 });
         doRegionTest(iter, { 1, 3, 3, 3 });
       }
-      THEN("Advance the iterator by regions that go out of one dimension "
-           "bound.") {
+      SECTION("Advance a regions that go out of one dimension bound.") {
         doRegionTest(iter, { 0, 1, 1, 18 });
       }
-      THEN("Advance the iterator by regions that go out of multiple dimension "
-           "bounds.") {
+      SECTION("Advance a region that go out of multiple dimension bounds.") {
         doRegionTest(iter, { 0, 9, 10, 18 });
       }
-      THEN("Advance the iterator by regions that triggers carries greater than "
-           "1.") {
+      SECTION("Advance a region that triggers carries greater than "
+              "1.") {
         doRegionTest(iter, { 0, 17, 10, 100 });
       }
     }
   }
 
-  GIVEN("Test tensor shape with alignment of 8 on the last dimension, and halo "
-        "regions of {1, 1} on the H and W dimensions.") {
+  SECTION("Test tensor shape with alignment of 8 on the last dimension, and "
+          "halo regions of {1, 1} on the H and W dimensions") {
     // After paddings, the shape becomes {4, 8, 8, 16}.
     TensorShape shape({ 4, 6, 6, 12 }, 8);
     std::vector<std::pair<int, int>> halo{
       { 0, 0 }, { 1, 1 }, { 1, 1 }, { 0, 0 }
     };
     TensorIndexIterator iter(shape, halo);
-    WHEN("Test halo region.") {
-      THEN("The initial position of the iterator is in the halo region of "
-           "indices {0, -1, -1, 0}.") {
-        REQUIRE(iter.getIndices() == std::vector<int>{ 0, -1, -1, 0 });
-        REQUIRE(iter.inHaloRegion() == true);
-      }
-      THEN("Advance to the first non-halo region.") {
-        iter += {0, 1, 1, 0};
-        REQUIRE(iter.getIndices() == std::vector<int>{ 0, 0, 0, 0 });
-        REQUIRE(iter.inHaloRegion() == false);
-      }
+    SECTION("Test halo regions") {
+      // The initial position of the iterator is in the halo region of indices
+      // {0, -1, -1, 0}.
+      REQUIRE(iter.getIndices() == std::vector<int>{ 0, -1, -1, 0 });
+      REQUIRE(iter.inHaloRegion() == true);
+      // Advance to the first non-halo region.
+      iter += { 0, 1, 1, 0 };
+      REQUIRE(iter.getIndices() == std::vector<int>{ 0, 0, 0, 0 });
+      REQUIRE(iter.inHaloRegion() == false);
     }
-    WHEN("Test regin-based advance.") {
-      THEN("Advance the iterator by regions that go out of one dimension "
-           "bound.") {
+    SECTION("Region-based advance") {
+      SECTION("Advance a regions that go out of one dimension bound.") {
         doRegionTest(iter, { 0, 1, 1, 18 });
       }
-      THEN("Advance the iterator by regions that go out of multiple dimension "
-           "bounds.") {
+      SECTION("Advance a region that go out of multiple dimension bounds.") {
         doRegionTest(iter, { 0, 9, 10, 18 });
       }
-      THEN("Advance the iterator by regions that triggers carries greater than "
-           "1.") {
+      SECTION("Advance a region that triggers carries greater than "
+              "1.") {
         doRegionTest(iter, { 0, 17, 10, 100 });
       }
     }
   }
 }
 
-SCENARIO("Test tensor region index iterator", "[region-index]") {
-  GIVEN("Test a region index iterator with same origins as the tensor it "
-        "iterates over.") {
+TEST_CASE("Test tensor region index iterator", "[region-index]") {
+  SECTION("A region index iterator with same origins as the tensor it "
+          "iterators over") {
     TensorShape shape({ 4, 6, 6, 12 });
     TensorRegionIndexIterator iter(shape, { 0, 0, 0, 0 }, { 3, 3, 3, 3 });
-    WHEN("Test region-based advance.") {
-      THEN("Advance the region iterator by a region that doesn't go beyond any "
-           "region bounds.") {
-        doRegionTest(iter, { 1, 1, 1, 1 });
-      }
-      THEN("Advance the region iterator by a region that doesn't go beyond "
-           "one region bound.") {
-        doRegionTest(iter, { 1, 1, 1, 4 });
-      }
-      THEN("Advance the region iterator by a region that doesn't go beyond "
-           "multiple region bounds.") {
-        doRegionTest(iter, { 1, 16, 8, 4 });
-      }
+    SECTION("Advance a region that doesn't go beyond any region bounds") {
+      doRegionTest(iter, { 1, 1, 1, 1 });
+    }
+    SECTION("Advance a region that goes beyond one region bound") {
+      doRegionTest(iter, { 1, 1, 1, 4 });
+    }
+    SECTION("Advance a region that go beyond multiple region bounds") {
+      doRegionTest(iter, { 1, 16, 8, 4 });
     }
   }
-  GIVEN("Test a region index iterator with different origins from the tensor "
-        "it iterates over.") {
+  SECTION("A region index iterator with different origins from the tensor it "
+          "iterators over") {
     TensorShape shape({ 4, 8, 8, 12 });
     TensorRegionIndexIterator iter(shape, { 1, 1, 2, 2 }, { 3, 3, 3, 3 });
-    WHEN("Test region-based advance.") {
-      THEN("Advance the region iterator by a region that doesn't go beyond any "
-           "region bounds.") {
-        doRegionTest(iter, { 1, 1, 1, 1 });
-      }
-      THEN("Advance the region iterator by a region that doesn't go beyond "
-           "one region bound.") {
-        doRegionTest(iter, { 1, 1, 1, 4 });
-      }
-      THEN("Advance the region iterator by a region that doesn't go beyond "
-           "multiple region bounds.") {
-        doRegionTest(iter, { 1, 16, 8, 4 });
-      }
-      THEN("Advance the iterator by regions that triggers carries greater than "
-           "1.") {
-        doRegionTest(iter, { 0, 0, 18, 13 });
-      }
+    SECTION("Advance a region that doesn't go beyond any region bounds") {
+      doRegionTest(iter, { 1, 1, 1, 1 });
+    }
+    SECTION("Advance a region that goes beyond one region bound") {
+      doRegionTest(iter, { 1, 1, 1, 4 });
+    }
+    SECTION("Advance a region that go beyond multiple region bounds") {
+      doRegionTest(iter, { 1, 16, 8, 4 });
+    }
+    SECTION("Advance the iterator by regions that triggers carries greater "
+            "than 1") {
+      doRegionTest(iter, { 0, 0, 18, 13 });
     }
   }
 }
